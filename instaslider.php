7498447f3bff1255a29fd7cf51bda7c4a42da6b3
Siddharth Ashok <siddharthashokg@gmail.com>
2017-10-13 19:46:06 +0530

    New settings options from currect Setting API

--- instaslider.php
+++ instaslider.php
@@ -19,9 +19,10 @@ function enqueue_css_instaslider() {
     wp_enqueue_script('instaslider-script-instafeed', plugin_dir_url(__FILE__) . '/js/instafeed.min.js', array( 'jquery' ), '1.0.0', true );
     wp_enqueue_script('instaslider-script', plugin_dir_url(__FILE__) . '/js/script.js', array( 'jquery' ), '1.0.0', true );
 
+    $options = get_option( 'insta_settings' );
     $script_params = array(
-      'instaUserID' => get_option('instaslider_insta_user_id'),
-      'instaAccessToken' => get_option('instaslider_insta_access_token'),
+      'instaUserID' => $options['insta_user_id'],
+      'instaAccessToken' => $options['insta_access_token'],
       'fileDirectory' => plugin_dir_url(__FILE__)
   );
 
@@ -29,45 +30,136 @@ function enqueue_css_instaslider() {
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_css_instaslider' );
 
-function instaslider_register_settings() {
-  add_option( 'instaslider_insta_user_id', '');
-  add_option( 'instaslider_insta_access_token', '');
-  register_setting( 'instaslider_options_group', 'instaslider_insta_user_id');
-  register_setting( 'instaslider_options_group', 'instaslider_insta_access_token');
-}
-add_action( 'admin_init', 'instaslider_register_settings' );
+// Settings Page options
+// generetaed from http://wpsettingsapi.jeroensormani.com/
 
-function instaslider_register_options_page() {
-  add_options_page('InstaSlider', 'InstaSlider', 'manage_options', 'instaslider', 'instaslider_options_page');
+function insta_add_admin_menu(  ) {
+	add_options_page( 'InstaSlider', 'InstaSlider', 'manage_options', 'instaslider', 'insta_options_page' );
 }
-add_action('admin_menu', 'instaslider_register_options_page');
+add_action( 'admin_menu', 'insta_add_admin_menu' );
 
-function instaslider_options_page()
-{
-?>
-  <div>
-  <?php screen_icon(); ?>
-  <h2>My Plugin Page Title</h2>
-  <form method="post" action="options.php">
-  <?php settings_fields( 'instaslider_options_group' ); ?>
-  <h3>This is my option</h3>
-  <p>Some text here.</p>
-  <table>
-  <tr valign="top">
-  <th scope="row"><label for="instaslider_insta_user_id">Instagram UserId</label></th>
-  <td><input type="text" id="instaslider_insta_user_id" name="instaslider_insta_user_id" value="<?php echo get_option('instaslider_insta_user_id'); ?>" /></td>
-  </tr>
-  <tr valign="top">
-  <th scope="row"><label for="instaslider_insta_access_token">Instagram Access Token</label></th>
-  <td><input type="text" id="instaslider_insta_access_token" name="instaslider_insta_access_token" value="<?php echo get_option('instaslider_insta_access_token'); ?>" /></td>
-  </tr>
-  </table>
-  <?php  submit_button(); ?>
-  </form>
-  </div>
-<?php
+function insta_settings_init(  ) {
+	register_setting( 'insta_options_group', 'insta_settings' );
+
+	add_settings_section(
+		'insta_options_section',
+		__( 'Your section description', 'wordpress' ),
+		'insta_settings_section_callback',
+		'insta_options_group'
+	);
+
+	add_settings_field(
+		'insta_user_id',
+		__( 'Instagram User ID', 'wordpress' ),
+		'insta_user_id_render',
+		'insta_options_group',
+		'insta_options_section'
+	);
+
+	add_settings_field(
+		'insta_access_token',
+		__( 'Instagram Access Token', 'wordpress' ),
+		'insta_access_token_render',
+		'insta_options_group',
+		'insta_options_section'
+	);
+
+	// add_settings_field(
+	// 	'insta_radio_field_2',
+	// 	__( 'Settings field description', 'wordpress' ),
+	// 	'insta_radio_field_2_render',
+	// 	'insta_options_group',
+	// 	'insta_options_section'
+	// );
+  //
+	// add_settings_field(
+	// 	'insta_radio_field_3',
+	// 	__( 'Settings field description', 'wordpress' ),
+	// 	'insta_radio_field_3_render',
+	// 	'insta_options_group',
+	// 	'insta_options_section'
+	// );
+  //
+	// add_settings_field(
+	// 	'insta_radio_field_4',
+	// 	__( 'Settings field description', 'wordpress' ),
+	// 	'insta_radio_field_4_render',
+	// 	'insta_options_group',
+	// 	'insta_options_section'
+	// );
+}
+add_action( 'admin_init', 'insta_settings_init' );
+
+function insta_user_id_render(  ) {
+	$options = get_option( 'insta_settings' );
+	?>
+	<input type='text' name='insta_settings[insta_user_id]' value='<?php echo $options['insta_user_id']; ?>'>
+	<?php
 }
 
+function insta_access_token_render(  ) {
+	$options = get_option( 'insta_settings' );
+	?>
+	<input type='text' name='insta_settings[insta_access_token]' value='<?php echo $options['insta_access_token']; ?>'>
+	<?php
+}
+
+/*
+function insta_radio_field_2_render(  ) {
+	$options = get_option( 'insta_settings' );
+	?>
+	<input type='radio' name='insta_settings[insta_radio_field_2]' <?php checked( $options['insta_radio_field_2'], 1 ); ?> value='1'>
+	<?php
+}
+
+
+function insta_radio_field_3_render(  ) {
+
+	$options = get_option( 'insta_settings' );
+	?>
+	<input type='radio' name='insta_settings[insta_radio_field_3]' <?php checked( $options['insta_radio_field_3'], 1 ); ?> value='1'>
+	<?php
+
+}
+
+
+function insta_radio_field_4_render(  ) {
+
+	$options = get_option( 'insta_settings' );
+	?>
+	<input type='radio' name='insta_settings[insta_radio_field_4]' <?php checked( $options['insta_radio_field_4'], 1 ); ?> value='1'>
+	<?php
+
+}
+*/
+
+function insta_settings_section_callback(  ) {
+
+	echo __( 'This section description', 'wordpress' );
+
+}
+
+
+function insta_options_page(  ) {
+
+	?>
+	<form action='options.php' method='post'>
+
+		<h2>InstaSlider</h2>
+
+		<?php
+		settings_fields( 'insta_options_group' );
+		do_settings_sections( 'insta_options_group' );
+		submit_button();
+		?>
+
+	</form>
+	<?php
+
+}
+
+
+// SHORTCODE
 function instafeed_shortcode_function() {
   $intafeed_template = '<section id="instagram"><div class="account-info">
     		<div class="account-info-wrap">
